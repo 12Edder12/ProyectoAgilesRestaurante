2f9114100306b4cc62e9e11523fb67fae5b9d478
davayme <dayme1937@uta.edu.ec>
2023-12-10 22:37:19 -0500

    Implementado verifición de cantidad de pedidos y se añadio un mensaje de confirmacion del que se subió el pedido

--- lib/pages/homeMesero/widget/custom_dialog.dart
+++ lib/pages/homeMesero/widget/custom_dialog.dart
@@ -15,7 +15,7 @@ class CustomDialog extends StatefulWidget {
 }
 
 class _CustomDialogState extends State<CustomDialog> {
- final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
   @override
   Widget build(BuildContext context) {
@@ -24,7 +24,7 @@ class _CustomDialogState extends State<CustomDialog> {
       insetPadding: const EdgeInsets.all(0),
       child: Center(
         child: Container(
-          margin: const  EdgeInsets.all(20),
+          margin: const EdgeInsets.all(20),
           decoration: BoxDecoration(
             color: Colors.white,
             border: Border.all(color: black),
@@ -39,7 +39,7 @@ class _CustomDialogState extends State<CustomDialog> {
                   children: [
                     Text(
                       'Pedido Nº ${DateTime.now().millisecondsSinceEpoch}',
-                      style:  const  TextStyle(
+                      style: const TextStyle(
                           color: black,
                           fontSize: 20,
                           fontWeight: FontWeight.bold),
@@ -47,10 +47,11 @@ class _CustomDialogState extends State<CustomDialog> {
                     ),
                   ],
                 ),
-                backgroundColor: kSecondaryColor, // Cambia esto al color que desees
+                backgroundColor:
+                    kSecondaryColor, // Cambia esto al color que desees
                 elevation: 0,
                 leading: IconButton(
-                  icon: const  Icon(Icons.close, color: Colors.black),
+                  icon: const Icon(Icons.close, color: Colors.black),
                   onPressed: () {
                     Navigator.of(context).pop();
                   },
@@ -61,13 +62,13 @@ class _CustomDialogState extends State<CustomDialog> {
                 child: SingleChildScrollView(
                   child: Column(
                     children: [
-                    Center(
+                      Center(
                         child: Text(
                           'Mesa Nº ${globals.mesaOrden.toString()}',
                           style: TextStyle(fontSize: 20),
                         ),
                       ),
-                    const    SizedBox(height: 10),
+                      const SizedBox(height: 10),
                     ]..addAll(
                         widget.pedidos.asMap().entries.map((entry) {
                           int idx = entry.key;
@@ -89,14 +90,14 @@ class _CustomDialogState extends State<CustomDialog> {
                               child: ListTile(
                                 title: Text(
                                   'Pizza: ${pedido.food.name}',
-                                  style:  const  TextStyle(fontSize: 20),
+                                  style: const TextStyle(fontSize: 20),
                                 ),
                                 subtitle: Text(
                                   'Cantidad: ${pedido.quantity}',
-                                  style:  const  TextStyle(fontSize: 16),
+                                  style: const TextStyle(fontSize: 16),
                                 ),
                                 trailing: IconButton(
-                                  icon:  const  Icon(Icons.edit),
+                                  icon: const Icon(Icons.edit),
                                   onPressed: () {
                                     final controller = TextEditingController();
                                     String errorMessage = '';
@@ -108,7 +109,8 @@ class _CustomDialogState extends State<CustomDialog> {
                                           builder: (BuildContext context,
                                               StateSetter setState) {
                                             return AlertDialog(
-                                              title:  const  Text('Editar cantidad'),
+                                              title:
+                                                  const Text('Editar cantidad'),
                                               content: Column(
                                                 mainAxisSize: MainAxisSize.min,
                                                 children: <Widget>[
@@ -123,38 +125,48 @@ class _CustomDialogState extends State<CustomDialog> {
                                                   ),
                                                   Text(
                                                     errorMessage,
-                                                    style:  const  TextStyle(
+                                                    style: const TextStyle(
                                                         color: Colors.red),
                                                   ),
                                                 ],
                                               ),
                                               actions: <Widget>[
                                                 TextButton(
-                                        child: const  Text('Confirmar'),
-                                        onPressed: () {
-                                          // Verifica si el campo de texto está vacío
-                                          if (controller.text.isEmpty) {
-                                            setState(() {
-                                              errorMessage = "Por favor, ingrese una cantidad";
-                                            });
-                                          } else {
-                                            int newQuantity = int.parse(controller.text);
-                                            // Verifica si la nueva cantidad es mayor que 10 o menor que 1
-                                            if (newQuantity > 10) {
-                                              setState(() {
-                                                errorMessage = "La cantidad no puede ser mayor que 10";
-                                              });
-                                            } else if (newQuantity < 1) {
-                                              setState(() {
-                                                errorMessage = "La cantidad no puede ser menor que 1";
-                                              });
-                                            } else {
-                                              pedido.quantity = newQuantity;
-                                              Navigator.of(context).pop();
-                                            }
-                                          }
-                                        },
-                                      ),
+                                                  child:
+                                                      const Text('Confirmar'),
+                                                  onPressed: () {
+                                                    // Verifica si el campo de texto está vacío
+                                                    if (controller
+                                                        .text.isEmpty) {
+                                                      setState(() {
+                                                        errorMessage =
+                                                            "Por favor, ingrese una cantidad";
+                                                      });
+                                                    } else {
+                                                      int newQuantity =
+                                                          int.parse(
+                                                              controller.text);
+                                                      // Verifica si la nueva cantidad es mayor que 10 o menor que 1
+                                                      if (newQuantity > 10) {
+                                                        setState(() {
+                                                          errorMessage =
+                                                              "La cantidad no puede ser mayor que 10";
+                                                        });
+                                                      } else if (newQuantity <
+                                                          1) {
+                                                        setState(() {
+                                                          errorMessage =
+                                                              "La cantidad no puede ser menor que 1";
+                                                        });
+                                                      } else {
+                                                        pedido.quantity =
+                                                            newQuantity;
+                                                        Navigator.of(context)
+                                                            .pop();
+                                                      }
+                                                    }
+                                                  },
+                                                ),
                                               ],
                                             );
                                           },
@@ -172,54 +184,75 @@ class _CustomDialogState extends State<CustomDialog> {
                 ),
               ),
               ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                 backgroundColor: kSecondaryColor, // color del fondo
-                  foregroundColor: Colors.white, // color del texto
-                  shape: RoundedRectangleBorder(
-                    borderRadius:
-                        BorderRadius.circular(10), // bordes redondeados
-                  ),
-                  padding:  const EdgeInsets.symmetric(
-                      horizontal: 30, vertical: 10), // padding
-                ), 
-            /*    ;*/
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                   const   Icon(Icons.check, color: black),
-                    Container(
-                      margin:  const EdgeInsets.only(left: 5),
-                      child: const  Text('Confirmar Pedido',
-                          style: TextStyle(fontSize: 20, color: black)),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: kSecondaryColor, // color del fondo
+                    foregroundColor: Colors.white, // color del texto
+                    shape: RoundedRectangleBorder(
+                      borderRadius:
+                          BorderRadius.circular(10), // bordes redondeados
                     ),
-                  ],
-                ),
-               /* onPressed: () {
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 30, vertical: 10), // padding
+                  ),
+                  /*    ;*/
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Icon(Icons.check, color: black),
+                      Container(
+                        margin: const EdgeInsets.only(left: 5),
+                        child: const Text('Confirmar Pedido',
+                            style: TextStyle(fontSize: 20, color: black)),
+                      ),
+                    ],
+                  ),
+                  /* onPressed: () {
                   for (Pedido pedido in globals.pedidos) {
                     print(
                         'Pedido: ${pedido.food.name} - Cantidad: ${pedido.quantity}');
                   }
                 },*/
-                onPressed: () async {
-          // Crear un nuevo documento en la colección 'pedidos'
-                DocumentReference pedidoRef = await _firestore.collection('pedidos').add({
-                'num_mesa': globals.mesaOrden,  // Puedes cambiar esto según el número de mesa
-                'detalle_pedido': {
-                 for (Pedido pedido in widget.pedidos)
-                pedido.food.id: {
-                  'cantidad': pedido.quantity,
-                },
-            },
-          });
+                  onPressed: () async {
+                    try {
+                      // Crear un nuevo documento en la colección 'pedidos'
+                      DocumentReference pedidoRef =
+                          await _firestore.collection('pedidos').add({
+                        'num_mesa': globals
+                            .mesaOrden, // Puedes cambiar esto según el número de mesa
+                        'detalle_pedido': {
+                          for (Pedido pedido in widget.pedidos)
+                            pedido.food.id: {
+                              'cantidad': pedido.quantity,
+                            },
+                        },
+                      });
 
-          print('Pedido confirmado con ID: ${pedidoRef.id}');
+                      // Muestra un SnackBar con un mensaje de éxito
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          content:
+                              Text('Pedido confirmado con ID: ${pedidoRef.id}'),
+                          backgroundColor:
+                              Colors.green, // color de fondo verde para éxito
+                        ),
+                      );
 
-                  widget.pedidos.clear();
-                  globals.orderCount = 0;
+                      widget.pedidos.clear();
+                      globals.orderCount = 0;
 
-                  Navigator.of(context).pop();
-                }
-              ),
+                      Navigator.of(context).pop();
+                    } catch (e) {
+                      // Muestra un SnackBar con un mensaje de error
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          content: Text(
+                              'Algo salió mal al subir el pedido a Firebase'),
+                          backgroundColor:
+                              Colors.red, // color de fondo rojo para error
+                        ),
+                      );
+                    }
+                  }),
             ],
           ),
         ),
