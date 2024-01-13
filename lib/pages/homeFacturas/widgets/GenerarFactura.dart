7bffcebff7428809de8ac81830f50a5296a891f4
WADEPAR621 <raulparedes20031221@gmail.com>
2024-01-13 14:38:04 -0500

    Implementacion: de funciones para poder crear los PDFs dentro del dispostivos y poder guardarlo

--- lib/pages/homeFacturas/widgets/GenerarFactura.dart
+++ lib/pages/homeFacturas/widgets/GenerarFactura.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:Pizzeria_Guerrin/pages/homeFacturas/widgets/PdfGenerator.dart';  // Asegúrate de importar la ubicación correcta de tu archivo PdfGenerator.dart
 
 class BotonEnviarFactura extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
-      onPressed: () {
-        _mostrarMensaje(context); // Llamamos a la función para mostrar el modal
+      onPressed: () async {
+        // Invocar al método para generar el PDF
+        await PdfGenerator.generatePDF();
+
+        // Mostrar un modal con un mensaje
+        _mostrarMensaje(context);
       },
       child: Text('Enviar Factura'),
     );
@@ -17,8 +22,8 @@ class BotonEnviarFactura extends StatelessWidget {
       context: context,
       builder: (BuildContext context) {
         return AlertDialog(
-          title: Text('Factura Enviada'),
-          content: Text('La factura ha sido enviada exitosamente.'),
+          title: Text('Factura Generada y Enviada'),
+          content: Text('La factura ha sido generada y enviada exitosamente.'),
           actions: <Widget>[
             TextButton(
               child: Text('Cerrar'),
---
+++ lib/pages/homeFacturas/widgets/PdfGenerator.dart
@@ -0,0 +1,32 @@
+import 'dart:io';
+import 'package:syncfusion_flutter_pdf/pdf.dart';
+import 'package:path_provider/path_provider.dart';
+
+class PdfGenerator {
+  static Future<void> generatePDF() async {
+    // Crear el documento PDF
+    PdfDocument document = PdfDocument();
+    PdfPage page = document.pages.add();
+    final PdfGraphics graphics = page.graphics;
+    graphics.drawString('Hola, este es un PDF generado desde Flutter', PdfStandardFont(PdfFontFamily.helvetica, 20));
+
+    try {
+      // Obtener el directorio de documentos (o cualquier otro directorio apropiado)
+      final directory = await getApplicationDocumentsDirectory();
+      final path = '${directory.path}/example.pdf';  // Ruta completa al archivo PDF
+
+      // Guardar el PDF en el directorio obtenido
+      final List<int> bytes = await document.save();
+      final File file = File(path);
+      await file.writeAsBytes(bytes);
+
+      // Mostrar un mensaje (esto es opcional, puedes eliminarlo si lo deseas)
+      print('PDF generado en $path');
+
+      // Cierra el documento
+      document.dispose();
+    } catch (error) {
+      print('Error al guardar el archivo PDF: $error');
+    }
+  }
+}
