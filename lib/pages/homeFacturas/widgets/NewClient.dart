170b3c2a50991077a0808be40153d9f06788ac95
davayme <dayme1937@uta.edu.ec>
2024-01-15 00:49:36 -0500

    Agregada validacion de cedula repetida

--- lib/pages/homeFacturas/widgets/NewClient.dart
+++ lib/pages/homeFacturas/widgets/NewClient.dart
@@ -31,7 +31,6 @@ class NuevoClienteModal extends StatelessWidget {
   final _formKey = GlobalKey<FormState>();
 
   Widget _formularioNuevoCliente(BuildContext context) {
-
     String cedula = '';
     String nombre = '';
     String apellido = '';
@@ -91,15 +90,39 @@ class NuevoClienteModal extends StatelessWidget {
                   if (_formKey.currentState!.validate()) {
                     _formKey.currentState!.save();
 
-                    // Aquí puedes enviar los datos a Firebase
-                    FirebaseFirestore.instance.collection('clientes').add({
-                      'ced_cli': cedula,
-                      'nom_cli': nombre,
-                      'ape_cli': apellido,
-                      'cor_cli': correo,
-                    });
+                    // Verificar si 'ced_cli' ya existe en la base de datos
+                    var query = await FirebaseFirestore.instance
+                        .collection('clientes')
+                        .where('ced_cli', isEqualTo: cedula)
+                        .get();
 
-                    Navigator.pop(context); // Cerrar el modal después de enviar
+                    if (query.docs.isEmpty) {
+                      // Si 'ced_cli' no existe en la base de datos, puedes enviar los datos
+                      FirebaseFirestore.instance.collection('clientes').add({
+                        'ced_cli': cedula,
+                        'nom_cli': nombre,
+                        'ape_cli': apellido,
+                        'cor_cli': correo,
+                      });
+
+                      Navigator.pop(
+                          context); // Cerrar el modal después de enviar
+                    } else {
+                      // Si 'ced_cli' ya existe en la base de datos, puedes mostrar un mensaje de error
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          content: Text(
+                            'El cliente ya está registrado',
+                            style: TextStyle(
+                                color: Colors.white), // Color de texto blanco
+                          ),
+                          backgroundColor: Colors.red, // Color de fondo rojo
+                        ),
+                      );
+
+                      Navigator.pop(
+                          context); // Cerrar el modal después de mostrar el mensaje de error
+                    }
                   }
                 },
                 child: Text('Guardar'),
